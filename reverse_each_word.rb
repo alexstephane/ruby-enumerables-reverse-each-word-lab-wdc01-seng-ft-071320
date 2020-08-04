6e356cdee1841a68798c6386cfe53bc3f8d4a276
alexstephane <alexngundji@gmail.comm.com>
2020-08-04 12:08:10 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(sentence)
+
   reversed = sentence.split(" ").collect do |word|
-  word.reverse
-end
-  reversed.join(" ")
+    word.reverse
+  end
+    reversed.join(" ")
+  
+ 
+
 end
